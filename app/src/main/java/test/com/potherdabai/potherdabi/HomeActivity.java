cbd70435c740b53b1678749787cc878bbd7ffe53
312soft <312soft@gmail.com>
2018-05-20 02:40:25 +0600

    start ui

---
+++ app/src/main/java/test/com/potherdabai/potherdabi/HomeActivity.java
@@ -0,0 +1,91 @@
+package test.com.potherdabai.potherdabi;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_drawer);
+        setUpUIMain();
+        setSupportActionBar(toolbar);
+
+        toggle = new ActionBarDrawerToggle(HomeActivity.this,drawerLayout,
+                toolbar,R.string.open_drawer,R.string.close_drawer);
+        navigationView.setNavigationItemSelectedListener(HomeActivity.this);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    private void setUpUIMain() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.home:
+                intent = new Intent(HomeActivity.this,HomeActivity.class);
+                startActivity(intent);
+
+                break;
+
+            case R.id.history:
+//                intent = new Intent(MainActivity.this,OverViewActivity.class);
+//                startActivity(intent);
+                Toast.makeText(HomeActivity.this, "History", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.contribution:
+//                intent = new Intent(MainActivity.this,BRAPrimarySchoolActivity.class);
+//                startActivity(intent);
+                Toast.makeText(HomeActivity.this, "Contribution", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.aboutus:
+//                intent = new Intent(MainActivity.this,BRAcPreprimaryActivity.class);
+//                startActivity(intent);
+                Toast.makeText(HomeActivity.this, "About us", Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
+
+    }
+}
